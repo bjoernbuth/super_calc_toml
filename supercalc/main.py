c3ea97ee42f0e1f87d6e1d3afa439d52ac9c05f9
bjoernbuth <bjoernbuth76@gmail.com>
2024-05-19 20:49:05 +0200

    testing click autocomplete - use long command name

--- supercalc/main.py
+++ supercalc/main.py
@@ -17,6 +17,19 @@ def read_number_from_file(filepath):
         return file.readline().strip()
 
 
+@simple.command()
+@click.help_option("--h", "-h", "--help")
+@click.option("--long_argument_name_01", type=int, help="First number")
+@click.option("--long_argument_name_02", type=int, help="Second number")
+def test_bash_completion_command(long_argument_name_01, long_argument_name_02):
+    """Test command for bash completion."""
+    click.echo(f"Arguments: {long_argument_name_01}, {long_argument_name_02}")
+    # click.echo("Test command for bash completion.")
+    # z = long_argument_name_01 + long_argument_name_02
+    # click.echo(f"Sum: {z}")
+    print("Hi")
+
+
 simple.add_command(main_group, name="si")
 simple.add_command(sci_grp, name="sc")
 
